a64e0a1c5a0a598dc61bab7e6247bb363d22d50d
Omar Gonzalez <om.gonzalez007@gmail.com>
2017-04-23 14:39:11 -0500

    Add files via upload

--- screen1.java
+++ screen1.java
@@ -1,4 +1,13 @@
 package sample;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
 
 /**
  * Created by Omar on 23/4/17.
--- screen2.java
+++ screen2.java
@@ -1,7 +1,25 @@
 package sample;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 
 /**
  * Created by Omar on 23/4/17.
  */
 public class screen2 {
+
+    
 }
