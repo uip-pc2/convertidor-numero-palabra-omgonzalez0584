a506eab0923d1d0c41ad71ad3dcc865e6e8d5b39
Omar Gonzalez <om.gonzalez007@gmail.com>
2017-04-23 13:54:08 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("screen1.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Convertir numero a Palabras");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
---
+++ screen2.java
@@ -0,0 +1,7 @@
+package sample;
+
+/**
+ * Created by Omar on 23/4/17.
+ */
+public class screen2 {
+}
